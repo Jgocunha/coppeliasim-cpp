4636a811bb9b4aea152dea02833b897d22cde495
Jgocunha <gasparcunha19@outlook.pt>
2023-06-19 11:22:45 +0100

    added all necessary files

---
+++ coppeliasim-cpp-client/coppeliasim-cpp-client.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include "coppeliasim-cpp-client.h"
+
+
+int main()
+{
+	CoppeliaSimClient client;
+
+	if (client.initialize())
+	{
+		client.log("You can log info to the Command line and CoppeliaSim!");
+	}
+	else
+	{
+		return 1;
+	}
+
+	return 0;
+}
